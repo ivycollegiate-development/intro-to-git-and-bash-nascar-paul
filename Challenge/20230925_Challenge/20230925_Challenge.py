600c51a92500867c2137550fea84f7616fe39d1b
Paul Jones <paul.tyler.jones@gmail.com>
2023-09-26 09:15:39 +0000

    Creates python script

---
+++ Challenge/20230925_Challenge/20230925_Challenge.py
@@ -0,0 +1,6 @@
+print('Please enter a name')# this line will not complete until the user presses the enter key
+name = input()
+if name:
+	print('Thank you for entering a name.')
+else:
+	print('You did not enter a name.')
